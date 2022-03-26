59ea3a6700ed5672804843d32b1046fc36f50cab
rubygrv <rubygrv19@gmail.com>
2022-03-26 16:28:22 +0700

    fix fb to niaga hoster

--- bin/helpers/databases/connection.js
+++ bin/helpers/databases/connection.js
@@ -2,16 +2,25 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'db4free.net',
-    port: '3306',
-    user: 'rubygrv',
-    password: 'Q089653204923q',
-    database:'rubygrvdb'
+    // DB db4free.net
+    // host: 'db4free.net',
+    // port: '3306',
+    // user: 'rubygrv',
+    // password: 'Q089653204923q',
+    // database:'rubygrvdb'
 
+    // DB local
     // host: 'localhost',
     // user: 'root',
     // password: '',
     // database:'desacipeundeuydb'
+
+    // DB Hosting
+    host: 'srv143.niagahoster.com',
+    port: '3306',
+    user: 'u1652720_kkndesacipeundeuy_root',
+    password: 'kkndesacipeundeuy2022',
+    database:'u1652720_desacipeundeuydb'
 });
 
 connection.connect((err) => {
